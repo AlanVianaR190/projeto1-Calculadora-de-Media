80e492f43cef7c4b33e1aa5333dbc0922f4279cd
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2025-01-21 14:24:14 -0300

    add

---
+++ metodos.js
@@ -0,0 +1,61 @@
+export class CalculadoraNotas{
+
+    // 
+    static labelNota(descricao){
+        let label = document.createElement("label");
+        label.innerHTML = descricao;
+        label.className = "rotulo";
+        return label;
+    }
+    
+    static inputNota(){
+        let input = document.createElement("input");
+        input.type = "number";
+        input.min = "0"; input.max = "10"; input.step = "0.1"; input.value = "required";
+        input.className = "numero";
+        return input;
+    }    
+
+    static inputCalcular(){
+        let input = document.createElement("input");
+        input.type = "button";
+        input.value = "Calcular";
+        input.className = "botao";
+        return input;
+    }
+
+    //
+    calcularPesoA1(valor){
+        return valor * 0.4;
+    }
+    
+    calcularPesoA5(valor){
+        return valor * 0.6;
+    }
+    
+    calcularMedia(valor1, valor2){
+        return (valor1 + valor2).toFixed(1);
+    }
+    
+    static calcularMediaPresencial(valorA1, valorA5){
+        let notaA1 = parseFloat(this.calcularPesoA1(valorA1));
+        let notaA5 = parseFloat(this.calcularPesoA5(valorA5));
+        let media = parseFloat(this.calcularMedia(notaA1, notaA5));
+        return media;
+    }
+
+    //
+    static sectionResultadoPresencial(){
+        let section = document.createElement("section");
+        section.innerHTML = "<h2>Resultado</h2>";
+
+        // let ul1 = document.createElement("ul");
+        // let calculadora = new CalculadoraNotas();
+        // let media = calculadora.calcularMediaPresencial(valor1, valor5);
+        // ul1.innerHTML = `<li>Sua media atual é: <b>${media}</b></li>`;
+        // section.appendChild(ul1);
+
+        section.className = "resultado";
+        return section;
+    }
+}
---
+++ program.js
@@ -0,0 +1,46 @@
+import { CalculadoraNotas } from './metodos.js';
+
+function presencial(){
+    let continueDiv = window.document.querySelector("div#continue");
+    continueDiv.style.textAlign = "left";
+    continueDiv.style.fontSize = "12px";
+    continueDiv.innerHTML = "<p>*Ao adicionar as notas, o sistema irá calcular a media e mostrar se o aluno foi aprovado, se sera necessário vazer a SUB(A6) ou reprovado. Por isso fique atento a nota e o peso da nota em trabalhos, atividades e provas.</p>";
+
+    let labelA1 = CalculadoraNotas.labelNota("Nota A1:");
+    continueDiv.appendChild(labelA1);
+    let inputA1 = CalculadoraNotas.inputNota();
+    continueDiv.appendChild(inputA1);
+
+    let labelA5 = CalculadoraNotas.labelNota("Nota A5: ");
+    continueDiv.appendChild(labelA5);   
+    let inputA5 = CalculadoraNotas.inputNota();
+    continueDiv.appendChild(inputA5);
+
+    let calcular = CalculadoraNotas.inputCalcular();
+    continueDiv.appendChild(calcular);
+
+    calcular.addEventListener("click", function(){
+        let resultadoExistente = window.document.querySelector("section.resultado");
+        if (resultadoExistente) {
+            resultadoExistente.remove();
+        }
+
+        // revisar esta variavel, melhor instanciar a classe CalculadoraNotas
+        let resultado = CalculadoraNotas.sectionResultadoPresencial(inputA1.value, inputA5.value);
+        continueDiv.appendChild(resultado);
+    });
+
+}
+
+function ead(){
+    let continueDiv = window.document.querySelector("div#continue");
+    continueDiv.style.textAlign = "left";
+    continueDiv.style.fontSize = "12px";
+    continueDiv.innerHTML = "<p>*Ao adicionar as notas, o sistema irá calcular a media e mostrar se o aluno foi aprovado, se sera necessário vazer a SUB(A6) ou reprovado.</p>";
+}
+
+// window.presencial = presencial;
+// window.ead = ead;
+window.document.querySelector("#botaoPresencial").addEventListener("click", presencial);
+window.document.querySelector("#botaoEad").addEventListener("click", ead);
+
